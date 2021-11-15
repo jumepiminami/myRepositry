76e0b1048a902469cccdb57ad2efad44b6bdae9d
jumpeiminami <92559616+jumepiminami@users.noreply.github.com>
2021-11-15 13:30:04 +0900

    Delete 入力時 directory

--- 入力時/script.js
+++
@@ -1,8 +0,0 @@
-// input要素オブジェクト
-const input = document.getElementById("input");
-// 値を埋め込む先
-const length = document.getElementById("length");
-
-input.addEventListener('input', (event) => {
-  length.innerText = event.target.value.length;
-});
